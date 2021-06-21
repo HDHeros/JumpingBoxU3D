781b116f483262c675517b5ce0aa28fc5d5de365
HDHeros <leodenx@gmail.com>
2021-06-21 11:53:37 +0300

    Финальная версия, работа над проектом продолжаться не будет

---
+++ JumpingBox/Assets/Scripts/Cube/PostProcScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcScript : MonoBehaviour
+{
+    private PostProcessVolume _ppVol;
+    private ChromaticAberration _chromatic;
+
+    private void Start()
+    {
+        _ppVol = GameObject.FindGameObjectWithTag("PPVol").GetComponent<PostProcessVolume>();
+        _ppVol.profile.TryGetSettings(out _chromatic);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<PlatformSolid>())
+        {
+            _chromatic.intensity.value = 0.5f;
+            StartCoroutine(TurnOffChromatic());
+        }
+
+    }
+
+    IEnumerator TurnOffChromatic()
+    {
+        yield return new WaitForSeconds(0.1f);
+        _chromatic.intensity.value = 0f;
+    }
+}
